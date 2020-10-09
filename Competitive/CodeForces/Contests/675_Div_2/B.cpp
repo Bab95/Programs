038234ade8f33365d2596cd47567d84557d78c3c
Bab95 <babnish95@gmail.com>
2020-10-09 22:39:15 +0530

    move

--- Competitive/CodeForces/Contests/675_Div_2/B.cpp
+++ Competitive/CodeForces/Contests/675_Div_2/B.cpp
@@ -10,66 +10,30 @@ void solve2(){
         }
     }
     long long int ans = 0;
-    for(int i=0;i<n/2;++i){
-        for(int j=0;j<m/2;++j){
+    for(int i=0;i<(n+1)/2;++i){
+        for(int j=0;j<(m+1)/2;++j){
             vector<int> current;
+            int x = n-i-1;
+            int y = m-j-1;
             current.push_back(mat[i][j]);
-            current.push_back(mat[n-i-1][j]);
-            current.push_back(mat[i][m-j-1]);
-            current.push_back(mat[n-i-1][m-j-1]);
+            if(x!=i){
+                current.push_back(mat[x][j]);
+            }
+            if(y!=j){
+                current.push_back(mat[i][y]);
+            }
+            if(x!=i&&y!=j){
+                current.push_back(mat[x][y]);
+            }
             sort(current.begin(),current.end());
-            int mid = current[2];
-            int cost = 0;
-            for(int k=0;k<current.size();++k){
-                cost += abs(mid-current[k]);
+            if(current.size()==2){
+                ans += (current[1]-current[0]);
+            }else if(current.size()==4){
+                ans += (current[3]+current[2]-current[1]-current[0]);
             }
-            int mid2 = current[1];
-            int cost2 = 0;
-            for(int k=0;k<current.size();++k){
-                cost2 += abs(mid2-current[k]);
-            }
-            ans += (long long int)min(cost,cost2);
         }
     }
 
-    if(n%2==1){
-        sort(mat[(n-1)/2].begin(),mat[(n-1)/2].end());
-        int mid = mat[(n-1)/2][m/2];
-        int cost = 0;
-        for(int i=0;i<m;++i){
-            cost += abs(mid-mat[(n-1)/2][i]);
-        }
-        int cost2 = INT_MAX;
-        if(m%2==0){
-            int mid2 = mat[(n-1)/2][m/2-1];
-            cost2 = 0;
-            for(int i=0;i<m;++i){
-                cost2 += abs(mid2-mat[(n-1)/2][i]);
-            }
-        }
-        ans += (long long int)min(cost,cost2);
-    }
-
-    if(m%2==1){
-        vector<int> tmp;
-        for(int i=0;i<n;++i){
-            tmp.push_back(mat[i][m/2]);
-        }
-        sort(tmp.begin(),tmp.end());
-        int cost = 0,cost2 = INT_MAX;
-        int mid = tmp[n/2];
-        for(int i=0;i<tmp.size();++i){
-            cost += abs(mid-tmp[i]);
-        }
-        if(n%2==0){
-            cost2 = 0;
-            int mid2 = tmp[n/2-1];
-            for(int i=0;i<tmp.size();++i){
-                cost2 += abs(mid2-tmp[i]);
-            }
-        }
-        ans += (long long int)min(cost,cost2);
-    }
     cout<<ans<<endl;
 }
 int main(){
